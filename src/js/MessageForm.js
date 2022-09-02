a93d897253a5aad6a22a485c0a5b17a28a4a10ba
Raphael Alampay <raphael.alampay@gmail.com>
2022-09-02 18:33:53 +0800

    formatted message form

--- src/js/MessageForm.js
+++ src/js/MessageForm.js
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 export default function MessageForm(props) {
     return (
         <>
-            <label>
-                First Name: 
-            </label>
-            <input
-            />
+            <div className="card">
+                <div className="card-body">
+                    <div className="form-group">
+                        <label>
+                            First Name: 
+                        </label>
+                        <input
+                            className="form-control"
+                        />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
